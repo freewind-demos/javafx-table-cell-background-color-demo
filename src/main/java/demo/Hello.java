cee15daec7044abd94f41a7f85591824af0ae395
freewind <nowindlee@gmail.com>
2018-07-23 13:55:55 +0800

    remove unused setEditable(true)

--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -1,30 +1,20 @@
 package demo;
 
 import javafx.application.Application;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleListProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.beans.property.SimpleStringProperty;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 import static javafx.collections.FXCollections.observableArrayList;
-
 
 public class Hello extends Application {
 
@@ -51,27 +41,22 @@ public class Hello extends Application {
 
     private Button createAddButton() {
         return new Button("Add Data") {{
-            setOnAction(new EventHandler<ActionEvent>() {
-                public void handle(ActionEvent event) {
-                    data.add(new Person("NEW", new Random().nextInt(100), Arrays.asList("date", "watermelon")));
-                }
-            });
+            setOnAction(event -> data.add(new Person("NEW", new Random().nextInt(100), Arrays.asList("date", "watermelon"))));
         }};
     }
 
     private TableView<Person> createTable() {
         return new TableView<Person>() {{
             getColumns().add(new TableColumn<Person, String>("Name") {{
-                setCellValueFactory(new PropertyValueFactory<Person, String>("name"));
+                setCellValueFactory(new PropertyValueFactory<>("name"));
             }});
             getColumns().add(new TableColumn<Person, Integer>("Number") {{
-                setCellValueFactory(new PropertyValueFactory<Person, Integer>("number"));
+                setCellValueFactory(new PropertyValueFactory<>("number"));
             }});
             getColumns().add(new TableColumn<Person, List<String>>("Fruits") {{
-                setCellValueFactory(new PropertyValueFactory<Person, List<String>>("fruits"));
+                setCellValueFactory(new PropertyValueFactory<>("fruits"));
             }});
             setItems(data);
-            setEditable(true);
         }};
     }
 }
--- src/main/java/demo/Person.java
+++ src/main/java/demo/Person.java
@@ -3,10 +3,8 @@ package demo;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleListProperty;
 import javafx.beans.property.SimpleStringProperty;
-import javafx.collections.ObservableList;
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static javafx.collections.FXCollections.observableArrayList;
@@ -16,10 +14,10 @@ public class Person {
     private final SimpleIntegerProperty number;
     private final SimpleListProperty<String> fruits;
 
-    public Person(String name, Integer number, List<String> fruits) {
+    Person(String name, Integer number, List<String> fruits) {
         this.name = new SimpleStringProperty(name);
         this.number = new SimpleIntegerProperty(number);
-        this.fruits = new SimpleListProperty<String>(observableArrayList(fruits));
+        this.fruits = new SimpleListProperty<>(observableArrayList(fruits));
     }
 
     public String getName() {
